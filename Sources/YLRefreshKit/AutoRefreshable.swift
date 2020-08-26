656ad0724f74f8075579e5c8a202dffd1dca4230
玉垒浮云 <yuleifuyunn@gmail.com>
2020-08-27 00:32:04 +0800

    refactor: 刷新库由 MJRefresh 改为 PullToRefreshKit

--- Sources/YLRefreshKit/AutoRefreshable.swift
+++ Sources/YLRefreshKit/AutoRefreshable.swift
@@ -5,87 +5,44 @@
 //  Created by 玉垒浮云 on 2020/8/22.
 //
 
-import MJRefresh
+import PullToRefreshKit
 import YLStateMachine
 
 public protocol AutoRefreshable {
     func setAutoRefresh<DataSource: DataSourceType, Operator: RefreshOperator<DataSource>>(
         with refreshStateMachine: StateMachine<Operator>
     )
-    
-    func setAutoRefresh<DataSource: DataSourceType, Operator: RefreshOperator<DataSource>>(
-        with refreshStateMachine: StateMachine<Operator>,
-        completion: @escaping () -> Void
-    )
 }
 
 extension UIScrollView: AutoRefreshable {
     public func setAutoRefresh<DataSource, Operator>(
-        with refreshStateMachine: StateMachine<Operator>,
-        completion: @escaping () -> Void
-    ) where DataSource : DataSourceType, Operator : RefreshOperator<DataSource> {
-        let header = MJRefreshNormalHeader(refreshingBlock: {
-            refreshStateMachine.trigger(.pullToRefresh) { [unowned self] in
-                self.mj_footer?.resetNoMoreData()
-                self.mj_header?.endRefreshing()
-                
-                completion()
-            }
-        })
-        
-        let footer = MJRefreshAutoNormalFooter(refreshingBlock: {
-            refreshStateMachine.trigger(.loadingMore) { [unowned self] in
-                if case .paginated = refreshStateMachine.currentState {
-                    // 有下一页
-                    self.mj_footer?.endRefreshing()
-                } else {
-                    // 没有下一页了
-                    self.mj_footer?.endRefreshingWithNoMoreData()
-                }
-                
-                completion()
-            }
-        })
-        
-        footer.stateLabel?.isHidden = true
-        mj_footer = footer
-        
-        header.isAutomaticallyChangeAlpha = true
-        header.lastUpdatedTimeLabel?.isHidden = true
-        header.stateLabel?.isHidden = true
-        mj_header = header
-        mj_header?.beginRefreshing()
-    }
-    
-    public func setAutoRefresh<DataSource, Operator>(
         with refreshStateMachine: StateMachine<Operator>
     ) where DataSource : DataSourceType, Operator : RefreshOperator<DataSource> {
-        let header = MJRefreshNormalHeader(refreshingBlock: {
-            refreshStateMachine.trigger(.pullToRefresh) { [unowned self] in
-                self.mj_footer?.resetNoMoreData()
-                self.mj_header?.endRefreshing()
-            }
-        })
-        
-        let footer = MJRefreshAutoNormalFooter(refreshingBlock: {
-            refreshStateMachine.trigger(.loadingMore) { [unowned self] in
-                if case .paginated = refreshStateMachine.currentState {
+        configRefreshHeader(container: self) { [unowned self] in
+            refreshStateMachine.trigger(.pullToRefresh) {
+                if case .error = refreshStateMachine.currentState {
+                    // 出错了
+                    self.switchRefreshHeader(to: .normal(.failure, 0.5))
+                } else if case .paginated = refreshStateMachine.currentState {
                     // 有下一页
-                    self.mj_footer?.endRefreshing()
+                    self.switchRefreshHeader(to: .normal(.success, 0.5))
                 } else {
                     // 没有下一页了
-                    self.mj_footer?.endRefreshingWithNoMoreData()
+                    self.switchRefreshFooter(to: .removed)
                 }
             }
-        })
+        }
         
-        footer.stateLabel?.isHidden = true
-        mj_footer = footer
-        
-        header.isAutomaticallyChangeAlpha = true
-        header.lastUpdatedTimeLabel?.isHidden = true
-        header.stateLabel?.isHidden = true
-        mj_header = header
-        mj_header?.beginRefreshing()
+        configRefreshFooter(container: self) { [unowned self] in
+            refreshStateMachine.trigger(.loadingMore) {
+                if case .populated = refreshStateMachine.currentState {
+                    // 有下一页或是出错了
+                    self.switchRefreshFooter(to: .normal)
+                } else {
+                    // 没有下一页了
+                    self.switchRefreshFooter(to: .removed)
+                }
+            }
+        }
     }
 }
