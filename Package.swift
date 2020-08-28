810ae10a4c356a8dfffbb2585725bf59362c4015
玉垒浮云 <yuleifuyunn@gmail.com>
2020-08-28 13:25:20 +0800

    修正库名错误

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "YLRefreshKit",
             dependencies: [
-                "PullToRefreshKit", "YLExtensions", "YLStateMachine"
+                "YLPullToRefreshKit", "YLExtensions", "YLStateMachine"
             ])
     ]
 )
