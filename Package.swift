5337d4d2832a49f5d15f7179e5fa8e3efa6f5c32
玉垒浮云 <yuleifuyunn@gmail.com>
2020-08-28 15:47:26 +0800

    Update Package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/YuLeiFuYun/PullToRefreshKit", from: "0.1.0"),
+        .package(url: "https://github.com/YuLeiFuYun/PullToRefreshKit", from: "1.0.0"),
         .package(url: "https://github.com/YuLeiFuYun/YLExtensions", from: "1.0.0"),
         .package(url: "https://github.com/YuLeiFuYun/YLStateMachine", from: "1.0.0")
     ],
